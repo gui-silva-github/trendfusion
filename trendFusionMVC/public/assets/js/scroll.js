70e92cb84dcbb1ba688fd6292d9137a9f1f258be
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-10-19 00:04:13 -0300

    Pasta pública do projeto, startando sessão e rodando as rotas

---
+++ trendFusionMVC/public/assets/js/scroll.js
@@ -0,0 +1,10 @@
+ScrollReveal({ 
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.author, .logo, .feed-new-editor, .head-side-right, .perfil', { origin: 'top' });
+ScrollReveal().reveal('.form, .feed-item, .feed-pagination, .follow, .fotos-page, .config-form', { origin: 'bottom' });
+ScrollReveal().reveal('.button, .input, .nav, .pesquisa, .info', { origin: 'left' });
+ScrollReveal().reveal('.img, .copyright, .banners, .fotos, .texto', { origin: 'right' });
