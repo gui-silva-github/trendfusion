10903dca73b0aaf94ae0c1b2f24a1853f1195b9b
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-10-19 00:08:22 -0300

    Configuração local do projeto

---
+++ trendFusionMVC/src/Config.php
@@ -0,0 +1,15 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = '/trendFusionMVC/public';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = 'localhost';
+    const DB_DATABASE = 'trendfusion';
+    CONST DB_USER = 'root';
+    const DB_PASS = '';
+
+    const ERROR_CONTROLLER = 'ErrorController';
+    const DEFAULT_ACTION = 'index';
+}
